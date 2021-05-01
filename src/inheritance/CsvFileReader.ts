97284f2245055613a60b295a3bb8c11bdab025d2
KrishnaTyagi01 <tyagikrishna38@gmail.com>
2021-05-01 15:22:40 +0530

    pushing code

---
+++ src/inheritance/CsvFileReader.ts
@@ -0,0 +1,22 @@
+// import fs from "fs";
+
+// export abstract class CsvFileReader<T> {
+//   data: T[] = [];
+
+//   constructor(public filename: string) {}
+
+//   //ISOLATE ALL THE CUSTOM LOGIC OF MAPPING ROW -> READ METHOD BECOMES REUSABLE
+//   abstract mapRow(row: string[]): T;
+
+//   read(): void {
+//     this.data = fs
+//       .readFileSync(this.filename, {
+//         encoding: "utf-8", // Tells readFileSync that we'll have text content inside the file
+//       })
+//       .split("\n")
+//       .map((row: string): string[] => {
+//         return row.split(",");
+//       })
+//       .map(this.mapRow);
+//   }
+// }
